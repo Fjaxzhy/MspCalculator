7a7342f334e72003c5ab302632f792869c470a01
MicTechStudio <33241583+Fjaxzhy@users.noreply.github.com>
2019-12-13 16:18:54 +0800

    Version_StableEdition 1.0

---
+++ MspCalculator/Help.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+namespace MspCalculator
+{
+    /// <summary>
+    /// Help.xaml 的交互逻辑
+    /// </summary>
+    public partial class Help : MetroWindow
+    {
+        public Help()
+        {
+            ResizeMode = ResizeMode.CanMinimize;
+            InitializeComponent();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
